f0cdc8c30c5e3071e7f1f5fab0e0ab643f57213a
EspancaVelhadaSilva <arthur.maximino.oliveira@escola.pr.gov.br>
2026-02-24 10:47:29 -0300

    Add files via upload

---
+++ ex12.js
@@ -0,0 +1,11 @@
+//Criar um programa com uma variável e mostrar se o número é maior ou menor que 10.
+let num1 = 9
+if (num1 > 10){
+    console.log (`O número ${num1} é maior que 10`)
+}
+else if (num1 < 10){
+    console.log (`O número ${num1} é menor que 10`)
+}
+else if (num1 = 10){
+    console.log(`O número ${num1} é igual que 10`)
+}
